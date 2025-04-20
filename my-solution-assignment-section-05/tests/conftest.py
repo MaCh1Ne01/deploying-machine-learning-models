7f31ce5ce68e36e825e1f59bfd8ba235d33b4942
Marcos Luyo <mluyoch01@gmail.com>
2025-04-20 16:48:14 -0500

    Assignment 4 Solution Folder was added.

---
+++ my-solution-assignment-section-05/tests/conftest.py
@@ -0,0 +1,19 @@
+import pytest
+from sklearn.model_selection import train_test_split
+
+from classification_model.config.core import config
+from classification_model.processing.data_manager import load_raw_dataset
+
+
+@pytest.fixture()
+def sample_input_data():
+    data = load_raw_dataset(file_name=config.app_config.raw_data_file)
+    X_train, X_test, y_train, y_test = train_test_split(
+        data.drop(config.model_config.target, axis=1),  # predictors
+        data[config.model_config.target],
+        test_size=config.model_config.test_size,
+        # we are setting the random seed here
+        # for reproducibility
+        random_state=config.model_config.random_state,
+    )
+    return X_test, y_test
